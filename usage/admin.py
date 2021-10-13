9d187b934409ad0b913ef9681453a8ca307216e5
Sayed Ahmad Sahim <sayedahmad.sahim@gmail.com>
2021-10-13 22:43:33 +0330

    models completed

--- usage/admin.py
+++ usage/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Usage,Usage_type
 # Register your models here.
+
+admin.site.register(Usage)
+admin.site.register(Usage_type)
--- usage/models.py
+++ usage/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from user.models import User
 # Create your models here.
+
+
+class Usage(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='usage')
+    usage_type_id=models.ForeignKey('Usage_type', on_delete=models.CASCADE, related_name='usage_type')
+    usage_at=models.DateTimeField()
+    amount=models.FloatField()
+
+    def __str__(self):
+        return str(self.usage_type_id)+" "+str(self.amount)
+
+
+class Usage_type(models.Model):
+    id=models.IntegerField(primary_key=True)
+    name=models.CharField(max_length=100)
+    unit=models.CharField(max_length=100, null=True)
+    factor=models.FloatField(max_length=50, null=True)
+
+    def __str__(self):
+        return str(self.id)+" "+self.name+" "+self.unit
