5e85d0289c5c3ea61398a2589bebd010517740c2
Sayed Ahmad Sahim <sayedahmad.sahim@gmail.com>
2021-10-15 16:01:37 +0330

    token authentication complated

--- usage/views.py
+++ usage/views.py
@@ -7,7 +7,10 @@ from rest_framework.generics import (
     ListCreateAPIView,
     UpdateAPIView,
 )
+
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Usage, UsageType
 from .serializer import UsageTypeSerializer, UsageSerializer
 # Create your views here
@@ -17,34 +20,41 @@ class UsageTypeView(ListAPIView):
     queryset = UsageType.objects.all()
     serializer_class = UsageTypeSerializer
     pagination_class = PageNumberPagination
+    permission_classes = (IsAuthenticated,)
 
 
 class UsageTypeCreateView(ListCreateAPIView):
     queryset = UsageType.objects.all()
     serializer_class = UsageSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class UsageTypeUpdateView(UpdateAPIView):
     queryset = UsageType.objects.all()
     serializer_class = UsageSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class UsageTypeDeleteView(DestroyAPIView):
     queryset = UsageType.objects.all()
     serializer_class = UsageSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class UsageCreateView(ListCreateAPIView):
     queryset = Usage.objects.all()
     serializer_class = UsageSerializer
     pagination_class = PageNumberPagination
+    permission_classes = (IsAuthenticated,)
 
 
 class UsageUpdateView(UpdateAPIView):
     queryset = Usage.objects.all()
     serializer_class = UsageSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class UsageDeleteView(DestroyAPIView):
     queryset = Usage.objects.all()
     serializer_class = UsageSerializer
+    permission_classes = (IsAuthenticated,)
--- user/models.py
+++ user/models.py
@@ -6,5 +6,6 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=255)
 
+
     def __str__(self):
         return str(self.id)+" "+self.name
